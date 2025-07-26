cf734c9d70e4ba235d87d4f3fbf07a28c0c25bbe
Your Name <your.email@example.com>
2025-07-26 13:37:51 +0530

    Functions Objects

---
+++ chrome/index.js
@@ -0,0 +1,14 @@
+//Log out "Button clicked!" when the user clicks the 
+
+// function saveLead(){
+//     console.log("button cliked")
+
+// }
+let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click",function(){
+    console.log("Button clicked from addEventListener")
+    
+})
+
+let inputBtn1 = document.getElementById("input-btn")
+
